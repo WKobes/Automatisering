6d513a80425b07d48bd8735f4c7b1ec852626a65
Alexander Green <alexander.green@logius.nl>
2022-01-14 12:16:58 +0100

    Update muffet-json.py

--- scripts/muffet-json.py
+++ scripts/muffet-json.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 errors = 0
 content = ''
@@ -6,10 +7,11 @@ content = ''
 with open('muffet.json') as file:
     data = json.load(file)
     for page in data:
-        content += '### ' + page['url'] + '\n'
-        for link in page['links']:
-            errors += 1
-            content += '* ' + link['url'] + ' `' + link['error'] + '`' + '\n\n'
+        if (re.search("\.nl\/[a-z]+\/[a-z]+\/$", page['url'])):
+            content += '### ' + page['url'] + '\n'
+            for link in page['links']:            
+                errors += 1
+                content += '* ' + link['url'] + ' `' + link['error'] + '`' + '\n\n'
 
 f = open('links.md', 'w')
 f.write('## ' + str(errors) + ' broken links\n\n')
