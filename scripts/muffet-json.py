9a6cb9baa5b2b3d9364911fd6f575471627c57ec
Alexander Green <alexander.green@logius.nl>
2022-01-31 11:04:48 +0100

    Double-check

--- scripts/muffet-json.py
+++ scripts/muffet-json.py
@@ -1,5 +1,6 @@
 import json
 import re
+import requests
 
 errors = 0
 content = ''
@@ -7,13 +8,20 @@ content = ''
 with open('muffet.json') as file:
     data = json.load(file)
     for page in data:
-        if (re.search("\.nl\/[a-z]+\/[a-z]+\/$|vv|github\.io", page['url'])):
+        if re.search("\.nl\/[a-z]+\/[a-z]+\/$|vv|github\.io", page['url']):
             content += '### ' + page['url'] + '\n'
-            for link in page['links']:            
+            for link in page['links']:                
+                try:  # Double-check
+                    r = requests.get(link['url'], timeout=5)
+                    if r.status_code == 200:
+                        content += '_Passed retest: ' + link['url'] + '_\n\n'
+                        continue  # Passed
+                except:
+                    print('Could not double-check ' + link['url'])
                 errors += 1
                 content += '* ' + link['url'] + ' `' + link['error'] + '`' + '\n\n'
 
 f = open('links.md', 'w')
 f.write('## ' + str(errors) + ' broken links\n\n')
-f.write(content)   
+f.write(content)
 f.close()
