d5331626322b4e1a50656f6639223b97044ed83e
Alexander Green <alexander.green@logius.nl>
2022-03-30 09:21:00 +0200

    Update labels.py

--- scripts/labels.py
+++ scripts/labels.py
@@ -9,17 +9,20 @@ labels = org.get_repo('Automatisering').get_labels()
 for repo in repos:
     if repo.name == 'Automatisering':
         continue
-    print('Checking ' + repo.name)
-    repo_labels = repo.get_labels()
-    for label in labels:
-        found = False
-        for repo_label in repo_labels:
-            if repo_label.name.upper() == label.name.upper():
-                if (repo_label.name != label.name) or (repo_label.description != label.description):
-                    repo_label.edit(name=label.name, color=label.color, description=label.description)
-                    print(f'Edited label \'{label.name}\' with description \'{label.description}\'')
-                found = True
-                break
-        if not found:
-            repo.create_label(name=label.name, color=label.color, description=label.description)
-            print(f'Created label \'{label.name}\'')
+    print('* Checking ' + repo.name)
+    try:
+        repo_labels = repo.get_labels()
+        for label in labels:
+            found = False
+            for repo_label in repo_labels:
+                if repo_label.name.upper() == label.name.upper():
+                    if (repo_label.name != label.name) or (repo_label.description != label.description):
+                        repo_label.edit(name=label.name, color=label.color, description=label.description)
+                        print(f'Edited label \'{label.name}\' with description \'{label.description}\'')
+                    found = True
+                    break
+            if not found:
+                repo.create_label(name=label.name, color=label.color, description=label.description)
+                print(f'Created label \'{label.name}\'')
+    except:
+        print('Could not update labels in ' + repo.name)
