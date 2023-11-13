4e1eb4f525f5123dc75487829b6398b64da59825
Alexander Green <alexander.green@logius.nl>
2023-11-13 11:11:34 +0100

    Do not count `301` as broken

--- scripts/muffet-json.py
+++ scripts/muffet-json.py
@@ -15,8 +15,9 @@ with open('muffet.json') as file:
             for link in page['links']:                
                 try:  # Double-check
                     r = requests.get(link['url'], timeout=5)
-                    if r.status_code == 200:
+                    if r.status_code == 200 or r.status_code == 301:
                         content += '\n_Passed retest: ' + link['url'] + '_\n'
+                        errors -= 1
                         continue  # Passed
                 except:
                     print('Could not double-check ' + link['url'])
