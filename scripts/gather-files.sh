03706a77dea88d208b309398ae40e821a0f4bade
Alexander Green <alexander.green@logius.nl>
2025-05-02 13:50:38 +0200

    Test for existence of `./media` (#43)
    * Test for existence of `./media`
    
    * Update scripts/gather-files.sh
    
    Co-authored-by: Tim van der Lippe <TimvdLippe@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tim van der Lippe <TimvdLippe@users.noreply.github.com>

--- scripts/gather-files.sh
+++ scripts/gather-files.sh
@@ -2,5 +2,5 @@ rm -rf ~/content
 mkdir ~/content
 cp ~/static/* ~/content/
 mv ~/content/snapshot.html ~/content/index.html
-mkdir ~/content/media/
-cp ./media/* ~/content/media/
+[ -d ./media ] && mkdir ~/content/media/
+[ -d ./media ] && cp ./media/* ~/content/media/
